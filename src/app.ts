8299145b8254c88da9db622514a52eb430751653
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-02-06 12:20:51 +0100

    feat: created new GH Action for deploy pipeline

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from "aws-lambda";
 
 export const handler = async function (_event: APIGatewayEvent, _context: Context): Promise<APIGatewayProxyResult> {
-  console.log("I am a Lambda function!")
+  console.log("I am a Lambda function! v2")
   return {
-    body: JSON.stringify({ message: "Hi there! I am a Lambda function!" }),
+    body: JSON.stringify({ message: "Hi there! I am a Lambda function! V2" }),
     headers: { "Content-Type": "application/json" },
     statusCode: 200,
   }
