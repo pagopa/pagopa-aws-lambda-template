4abeab702c701effeb53ce30e36ae8d1a531a7bc
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-02-07 09:38:59 +0100

    fix: updated Docker image identifier
    With this commit, the complete name of the Docker image to be pushed in ECR is changed in order to set the application version and the SHA identifier.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from "aws-lambda";
 
 export const handler = async function (_event: APIGatewayEvent, _context: Context): Promise<APIGatewayProxyResult> {
-  console.log("I am a Lambda function! v2")
+  console.log("I am a Lambda function!")
   return {
-    body: JSON.stringify({ message: "Hi there! I am a Lambda function! V2" }),
+    body: JSON.stringify({ message: "Hi there! I am a Lambda function!" }),
     headers: { "Content-Type": "application/json" },
     statusCode: 200,
   }
