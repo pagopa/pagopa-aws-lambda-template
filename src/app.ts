2c6eb43a38bd98d531e45f74e4955bd931b9acc5
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-02-07 16:50:19 +0100

    feat: created new GH Action for code-review pipeline

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from "aws-lambda";
 
-export const handler = async function (_event: APIGatewayEvent, _context: Context): Promise<APIGatewayProxyResult> {
-  console.log("I am a Lambda function!")
+/* eslint-disable prefer-arrow/prefer-arrow-functions */
+export const handler = async function (
+  _event: APIGatewayEvent,
+  _context: Context
+): Promise<APIGatewayProxyResult> {
   return {
-    body: JSON.stringify({ message: "Hi there! I am a Lambda function!" }),
+    body: JSON.stringify({ message: "Hi there! I am a Lambda function! V2" }),
     headers: { "Content-Type": "application/json" },
     statusCode: 200,
-  }
+  };
 };
